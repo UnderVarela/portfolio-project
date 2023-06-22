dfb3469756681165aaf1802200540fcefb4eb31c
UnderVarela <under.varela@gmail.com>
2023-06-22 15:00:52 +0200

    Creado Card Component. HomePage2

---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Card(props) {
+  const { imageSrc, title, description, link } = props
+
+  return (
+    <div className='p-4 overflow-hidden transition duration-300 transform bg-gray-400 rounded-lg shadow-lg hover:scale-105'>
+      <a href={link} target='_blank' rel='noopener noreferrer'>
+        <img className='object-cover w-full h-48' src={imageSrc} alt={title} />
+      </a>
+      <div className='p-4'>
+        <h3 className='mb-2 text-xl font-semibold'>{title}</h3>
+        <p className='text-gray-700'>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+Card.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+}
+
+export default Card
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,50 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Card from '../components/Card';
 
 export function HomePage() {
   return (
     <>
-    <h2 className='text-4xl'>Home Page</h2>
-    <div className='grid grid-cols-3 gap-4 mx-4 mt-8'>
-      <div className='p-4 overflow-hidden transition duration-300 transform bg-gray-400 rounded-lg shadow-lg hover:scale-105'>
-        <img
-          className='object-cover w-full h-48'
-          src='src/images/istockphoto-622064544-612x612.jpg'
-          alt='Trabajo 1'
+      <h2 className='text-4xl'>Mis movidas</h2>
+      <div className='grid grid-cols-3 gap-4 mx-4 mt-8'>
+        <Card
+          imageSrc='src/images/istockphoto-622064544-612x612.jpg'
+          title='Tecnico audiovisual'
+          description='Descripción del trabajo'
+          link='https://www.example.com'
         />
-        <div className='p-4'>
-          <h3 className='mb-2 text-xl font-semibold'>Tecnico audiovisual</h3>
-          <p className='text-gray-700'>
-            Descripción del trabajo 
-          </p>
-        </div>
-      </div>
-      <div className='overflow-hidden transition duration-300 transform bg-gray-400 rounded-lg shadow-lg hover:scale-105'>
-        <img
-          className='object-cover w-full h-48'
-          src='src/images/IMDB_recortado.jpg'
-          alt='Trabajo 2'
+        <Card
+          imageSrc='src/images/IMDB_recortado.jpg'
+          title='Mi perfil de IMDB'
+          description='Sonido. Dirección de fotografía y cámara. Arte.'
+          link='https://www.imdb.com/name/nm6244375/?ref_=fn_al_nm_1'
         />
-        <div className='p-4'>
-          <h3 className='mb-2 text-xl font-semibold'>Mi perfil de IMDB</h3>
-          <p className='text-gray-700'>
-            Descripción del trabajo
-          </p>
-        </div>
-      </div>
-      <div className='p-4 overflow-hidden transition duration-300 transform bg-gray-400 rounded-lg shadow-lg hover:scale-105'>
-        <img
-          className='object-cover w-full h-48'
-          src='src/images/yo.jpg'
-          alt='Trabajo 3'
+        <Card
+          imageSrc='src/images/yo.jpg'
+          title='Sobre mi'
+          description='Descripción del trabajo 3'
+          link='https://www.example.com'
         />
-        <div className='p-4'>
-          <h3 className='mb-2 text-xl font-semibold'>Sobre mi</h3>
-          <p className='text-gray-700'>
-            Descripción del trabajo 3
-          </p>
-        </div>
       </div>
-    </div>
-  </>
+    </>
   )
 }
+
+
